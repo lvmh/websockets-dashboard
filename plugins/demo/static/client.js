b1790f58660b7c7f4d7b750667ac913e1c76f12b
Josh Corich <joshc@catalyst.net.nz>
2015-04-20 14:07:52 +1200

    Adding fadeout example to the demo, reload every 5 minutes if the connection is lost.

--- plugins/demo/static/client.js
+++ plugins/demo/static/client.js
@@ -1,51 +1,60 @@
 plugins.demo = {
 
-    html: null,
+  html: null,
 
-    start: function() {
-        dashletbase = ('<div class="plugin" id="demo"> \
+  start: function() {
+    dashletbase = ('<div class="plugin" id="demo"> \
                 <h1>Demo</h1> \
                 <div class="lastchange"></div> \
                 </div>');
-        $('div#body').append(dashletbase);
-        $.getScript(base_url + '/plugin/demo/jquery.timeago.js');
-    },
+    $('div#body').append(dashletbase);
+    $.getScript(base_url + '/plugin/demo/jquery.timeago.js');
 
-    receiveData: function(data) {
-        // Check for an array message (is array?)
-        if ($.isArray(data)) {
-            // Only care about the newest message
-            data = data.pop();
-        }
+    // Stale Content Plugin
+    // #demo = wrapping div for content
+    // .fadeTo(20000, "0.33"); replace 20000 with the amount of time in MS that you want it to take before it goes stale
+    // on Dom update, sets opacity back to 1 (in case it is stale), then initiates a timer to make the content stale. 1 day = 86400000, 5 minutes = 300000
 
-        var dashletdiv = $('div#demo');
-        var existing = $('li', dashletdiv).clone();
+    $('#demo').bind("DOMSubtreeModified", function() {
+      $(this).css("opacity", "1").stop().fadeTo(20000, "0.33");
+    });
+  },
 
-
-        // Last change
-        var lastchange = $('div.lastchange', dashletdiv);
-        if (data['lastchange']) {
-            var c = new Date();
-            c.setTime(data['lastchange'] * 1000);
-            lastchange.html('Last change: '+c.toDateString()+' '+c.toTimeString());
-        }
-        divid = 'demodatadiv'
-        escapeddivid = jq( divid ) // To escape any '.', ':' etc.
-        var datadiv = $( '#' + escapeddivid )
-        var newdatadiv = $('<div id="'+divid+'"><span class="datadata">'+ data['data'] +'</span></div>');
-        if (datadiv.length == 0) {
-            // There was no div of that id in dashletdiv.
-            // Create one.
-            dashletdiv.append(newdatadiv) // Put the new div at the end of the dashlet content.
-        } else {
-            // Clear out the existing one.
-            datadiv.replaceWith(newdatadiv)
-        }
-        datadiv = newdatadiv
+  receiveData: function(data) {
+    // Check for an array message (is array?)
+    if ($.isArray(data)) {
+      // Only care about the newest message
+      data = data.pop();
     }
+
+    var dashletdiv = $('div#demo');
+    var existing = $('li', dashletdiv).clone();
+
+
+    // Last change
+    var lastchange = $('div.lastchange', dashletdiv);
+    if (data['lastchange']) {
+      var c = new Date();
+      c.setTime(data['lastchange'] * 1000);
+      lastchange.html('Last change: ' + c.toDateString() + ' ' + c.toTimeString());
+    }
+    divid = 'demodatadiv'
+    escapeddivid = jq(divid) // To escape any '.', ':' etc.
+    var datadiv = $('#' + escapeddivid)
+    var newdatadiv = $('<div id="' + divid + '"><span class="datadata">' + data['data'] + '</span></div>');
+    if (datadiv.length == 0) {
+      // There was no div of that id in dashletdiv.
+      // Create one.
+      dashletdiv.append(newdatadiv) // Put the new div at the end of the dashlet content.
+    } else {
+      // Clear out the existing one.
+      datadiv.replaceWith(newdatadiv)
+    }
+    datadiv = newdatadiv
+  }
 }
 
 // Escape dom 'id' values - replace '.' with '\\.'.
-function jq( myid ) {
-    return myid.replace( /(:|\.|\[|\]|,)/g, "\\$1" );
+function jq(myid) {
+  return myid.replace(/(:|\.|\[|\]|,)/g, "\\$1");
 }
--- server/static/base.js
+++ server/static/base.js
@@ -15,237 +15,243 @@
 var plugins = {};
 
 $(document).ready(function() {
-    if (!window.console) window.console = {};
-    if (!window.console.log) window.console.log = function() {};
+  if (!window.console) window.console = {};
+  if (!window.console.log) window.console.log = function() {};
 
-    // Set the name of the hidden property
-    updater.config.hidden = false;
-    var h = ['hidden', 'mozHidden', 'msHidden', 'webkitHidden'];
-    for (var i = 0; i < h.length; i++) {
-        if (typeof document[h[i]] !== "undefined") {
-            updater.config.hidden = h[i];
-            break;
-        }
+  // Set the name of the hidden property
+  updater.config.hidden = false;
+  var h = ['hidden', 'mozHidden', 'msHidden', 'webkitHidden'];
+  for (var i = 0; i < h.length; i++) {
+    if (typeof document[h[i]] !== "undefined") {
+      updater.config.hidden = h[i];
+      break;
     }
+  }
 
-    for (p in plugins) {
-        plugin = plugins[p];
-        console.log('Start plugin '+p);
-        plugin.start();
+  for (p in plugins) {
+    plugin = plugins[p];
+    console.log('Start plugin ' + p);
+    plugin.start();
+  }
+
+  updater.start();
+
+  // add listener for view toggle
+  $('#view-toggler').click(function(e) {
+    if (!$('body').hasClass('dashboard-fullscreen')) {
+      $('body').addClass('dashboard-fullscreen');
+
+      transitioner.init();
+      transitioner.add_listeners();
+    } else {
+      transitioner.stop();
+      transitioner.remove_listeners();
+
+      $('body').removeClass('dashboard-fullscreen');
+
+      // show all plugins
+      $('div.plugin').show();
     }
-
-    updater.start();
-
-    // add listener for view toggle
-    $('#view-toggler').click(function (e) {
-        if (!$('body').hasClass('dashboard-fullscreen')) {
-            $('body').addClass('dashboard-fullscreen');
-
-            transitioner.init();
-            transitioner.add_listeners();
-        } else {
-            transitioner.stop();
-            transitioner.remove_listeners();
-
-            $('body').removeClass('dashboard-fullscreen');
-
-            // show all plugins
-            $('div.plugin').show();
-        }
-    });
+  });
 });
 
 // Plugin transitioner
 var transitioner = {
-    config: {
-        tdelay: 10000,  // delay between transitions
-        edelay: 1000,   // effect delay (cannot be more than tdelay!)
-        interval: 0,
-        paused: 0,
-        firstplugin: null,
-        lastplugin: null,
-        toshow: null // plugin to show
-    },
+  config: {
+    tdelay: 10000, // delay between transitions
+    edelay: 1000, // effect delay (cannot be more than tdelay!)
+    interval: 0,
+    paused: 0,
+    firstplugin: null,
+    lastplugin: null,
+    toshow: null // plugin to show
+  },
 
-    init: function() {
-        var obj = this;
+  init: function() {
+    var obj = this;
 
-        // hide all plugins
-        $('.dashboard-fullscreen div.plugin').hide();
+    // hide all plugins
+    $('.dashboard-fullscreen div.plugin').hide();
 
-        obj.config.firstplugin = $('.dashboard-fullscreen div.plugin:first');
-        obj.config.lastplugin = $('.dashboard-fullscreen div.plugin:last');
+    obj.config.firstplugin = $('.dashboard-fullscreen div.plugin:first');
+    obj.config.lastplugin = $('.dashboard-fullscreen div.plugin:last');
 
-        // show the first plugin immediately
-        obj.config.firstplugin.fadeIn(obj.config.edelay);
+    // show the first plugin immediately
+    obj.config.firstplugin.fadeIn(obj.config.edelay);
 
-        // start on interval (if more than one plugin)
-        if (obj.config.firstplugin.next().length) {
-            obj.config.toshow = obj.config.firstplugin.next();
-            obj.start();
-        } else {
-            obj.config.toshow = obj.config.firstplugin;
-        }
-    },
-
-    add_listeners: function() {
-        var obj = this;
-
-        // add keyboard listeners
-        $('body.dashboard-fullscreen').keydown(function (e) {
-            if (e.which == 40) {  // down arrow for pause
-                obj.config.paused = !obj.config.paused;
-                if (obj.config.paused) {
-                    obj.stop();
-                    $('.footer-center').append('<p id="transitioner-paused-indicator">Paused</p>');
-                    $('#transitioner-paused-indicator').effect("pulsate", { times:77 }, 77777);
-                } else {
-                    $('#transitioner-paused-indicator').remove();
-                    obj.start();
-                }
-                return false;  // prevent repainting that might happen
-            } else {
-                // any other keypress should unpause
-                $('#transitioner-paused-indicator').remove();
-                obj.config.paused = 0;
-            }
-
-            if (e.which == 39) {  // rigth arrow
-                //  stop transitioner temporarily
-                obj.stop();
-
-                // call transition manually
-                obj.plugin_transition();
-
-                // start transitioner
-                obj.start();
-            } else if (e.which == 37) {  // left arrow
-                //  stop transitioner temporarily
-                obj.stop();
-
-                // call transition manually
-                obj.config.toshow = obj.get_prev();
-                obj.plugin_transition();
-
-                // start transitioner
-                obj.start();
-            }
-        });
-    },
-
-    remove_listeners: function() {
-        $('body.dashboard-fullscreen').off('keydown');
-    },
-
-    // show the current plugin and set next one to show
-    plugin_transition: function() {
-        var obj = this;
-
-        // hide all plugins
-        $('.dashboard-fullscreen div.plugin').hide();
-
-        // show current plugin
-        obj.config.toshow.fadeIn(obj.config.edelay);
-
-        // set next plugin to show
-        obj.config.toshow = obj.get_next();
-    },
-
-    get_next: function() {
-        var next = this.config.toshow.next();
-
-        if (next.length) {
-            return next;
-        } else {
-            // wrap around :D
-            return this.config.firstplugin;
-        }
-    },
-
-    get_prev: function() {
-        var prev = this.config.toshow.prev().prev();  // we need to go back 2 as toshow is the next one
-
-        if (prev.length) {
-            return prev;
-        } else if (this.config.toshow == this.config.firstplugin) {
-            // little hack to get wrapping to work
-            return this.config.lastplugin.prev();
-        } else {
-            // wrap around :D
-            return this.config.lastplugin;
-        }
-    },
-
-    stop: function() {
-        var obj = this;
-
-        // remove the transitioning interval
-        clearInterval(obj.config.interval);
-        obj.config.interval = 0;
-    },
-
-    start: function() {
-        var obj = this;
-
-        // add new interval (if more than one plugin)
-        if (obj.config.firstplugin.next().length) {
-            tinterval = obj.config.tdelay + obj.config.edelay;
-            obj.config.interval = setInterval(function() {obj.plugin_transition();}, obj.config.tdelay);
-        }
+    // start on interval (if more than one plugin)
+    if (obj.config.firstplugin.next().length) {
+      obj.config.toshow = obj.config.firstplugin.next();
+      obj.start();
+    } else {
+      obj.config.toshow = obj.config.firstplugin;
     }
+  },
+
+  add_listeners: function() {
+    var obj = this;
+
+    // add keyboard listeners
+    $('body.dashboard-fullscreen').keydown(function(e) {
+      if (e.which == 40) { // down arrow for pause
+        obj.config.paused = !obj.config.paused;
+        if (obj.config.paused) {
+          obj.stop();
+          $('.footer-center').append('<p id="transitioner-paused-indicator">Paused</p>');
+          $('#transitioner-paused-indicator').effect("pulsate", {
+            times: 77
+          }, 77777);
+        } else {
+          $('#transitioner-paused-indicator').remove();
+          obj.start();
+        }
+        return false; // prevent repainting that might happen
+      } else {
+        // any other keypress should unpause
+        $('#transitioner-paused-indicator').remove();
+        obj.config.paused = 0;
+      }
+
+      if (e.which == 39) { // rigth arrow
+        //  stop transitioner temporarily
+        obj.stop();
+
+        // call transition manually
+        obj.plugin_transition();
+
+        // start transitioner
+        obj.start();
+      } else if (e.which == 37) { // left arrow
+        //  stop transitioner temporarily
+        obj.stop();
+
+        // call transition manually
+        obj.config.toshow = obj.get_prev();
+        obj.plugin_transition();
+
+        // start transitioner
+        obj.start();
+      }
+    });
+  },
+
+  remove_listeners: function() {
+    $('body.dashboard-fullscreen').off('keydown');
+  },
+
+  // show the current plugin and set next one to show
+  plugin_transition: function() {
+    var obj = this;
+
+    // hide all plugins
+    $('.dashboard-fullscreen div.plugin').hide();
+
+    // show current plugin
+    obj.config.toshow.fadeIn(obj.config.edelay);
+
+    // set next plugin to show
+    obj.config.toshow = obj.get_next();
+  },
+
+  get_next: function() {
+    var next = this.config.toshow.next();
+
+    if (next.length) {
+      return next;
+    } else {
+      // wrap around :D
+      return this.config.firstplugin;
+    }
+  },
+
+  get_prev: function() {
+    var prev = this.config.toshow.prev().prev(); // we need to go back 2 as toshow is the next one
+
+    if (prev.length) {
+      return prev;
+    } else if (this.config.toshow == this.config.firstplugin) {
+      // little hack to get wrapping to work
+      return this.config.lastplugin.prev();
+    } else {
+      // wrap around :D
+      return this.config.lastplugin;
+    }
+  },
+
+  stop: function() {
+    var obj = this;
+
+    // remove the transitioning interval
+    clearInterval(obj.config.interval);
+    obj.config.interval = 0;
+  },
+
+  start: function() {
+    var obj = this;
+
+    // add new interval (if more than one plugin)
+    if (obj.config.firstplugin.next().length) {
+      tinterval = obj.config.tdelay + obj.config.edelay;
+      obj.config.interval = setInterval(function() {
+        obj.plugin_transition();
+      }, obj.config.tdelay);
+    }
+  }
 }
 
 // Websocket handler
 var updater = {
-    config: {},
-    socket: null,
+  config: {},
+  socket: null,
 
-    start: function() {
-        // Create websocket
-        var url = "ws://" + location.host + "/chatsocket";
-        if ("WebSocket" in window) {
-            updater.socket = new WebSocket(url);
-        } else {
-            updater.socket = new MozWebSocket(url);
-        }
-
-        // On websocket message receive
-        updater.socket.onmessage = function(event) {
-            // Get data
-            var data = JSON.parse(event.data);
-
-            // Loop through messages
-            updater.routeMessage(data);
-        };
-
-        // On websocket close
-        updater.socket.onclose = function() {
-            $('body').prepend($('<div class="error">Connection lost, please refresh browser</div>'));
-        };
-    },
-
-    // Route received message
-    routeMessage: function(data) {
-        // Get data
-        var plugin = $(data.html).data('plugin');
-        var content = $(data.html).text();
-
-        if (!plugins[plugin]) {
-            return;
-        }
-
-        // Check if dashboard is not hidden, or if it is that it supports background data
-        if (!document[updater.config.hidden] || updater.pluginSupportsBackgroundData(plugin)) {
-            // Push to plugin
-            console.log('Data received for '+plugin+' plugin');
-            plugins[plugin].receiveData(jQuery.parseJSON(content));
-        }
-    },
-
-    // Check plugin supports receiving background data
-    pluginSupportsBackgroundData: function(plugin) {
-        return typeof plugins[plugin].noBackgroundData === "undefined";
+  start: function() {
+    // Create websocket
+    var url = "ws://" + location.host + "/chatsocket";
+    if ("WebSocket" in window) {
+      updater.socket = new WebSocket(url);
+    } else {
+      updater.socket = new MozWebSocket(url);
     }
+
+    // On websocket message receive
+    updater.socket.onmessage = function(event) {
+      // Get data
+      var data = JSON.parse(event.data);
+
+      // Loop through messages
+      updater.routeMessage(data);
+    };
+
+    // On websocket close
+    updater.socket.onclose = function() {
+      $('body').prepend($('<div class="error">Connection lost, please refresh browser</div>'));
+      //Refresh page after Time
+      setInterval(location.reload(), 300000);
+    };
+  },
+
+  // Route received message
+  routeMessage: function(data) {
+    // Get data
+    var plugin = $(data.html).data('plugin');
+    var content = $(data.html).text();
+
+    if (!plugins[plugin]) {
+      return;
+    }
+
+    // Check if dashboard is not hidden, or if it is that it supports background data
+    if (!document[updater.config.hidden] || updater.pluginSupportsBackgroundData(plugin)) {
+      // Push to plugin
+      console.log('Data received for ' + plugin + ' plugin');
+      plugins[plugin].receiveData(jQuery.parseJSON(content));
+    }
+  },
+
+  // Check plugin supports receiving background data
+  pluginSupportsBackgroundData: function(plugin) {
+    return typeof plugins[plugin].noBackgroundData === "undefined";
+  }
 };
 
 var videoElement = document.getElementById("videoElement");
